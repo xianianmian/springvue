a8aaf989563383399d76775ba6ba657c5fadbaed
邝利豪 <10530797+xianianmian@user.noreply.gitee.com>
2023-04-25 23:16:48 +0800

    "day1"

--- src/main.js
+++ src/main.js
@@ -3,6 +3,12 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+// Vue.use(ElementUI,{size:'small'})
+Vue.use(ElementUI)
+import './assets/gloable.css'
+
 Vue.config.productionTip = false
 
 new Vue({
