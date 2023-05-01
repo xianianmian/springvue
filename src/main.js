4c308484750e8cefb135158183d9c530917783ee
邝利豪 <10530797+xianianmian@user.noreply.gitee.com>
2023-05-01 16:04:39 +0800

    登陆注册功能完成

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import store from './store'
 import request from "@/utils/request";
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
+// Vue.use(Vuex)
 Vue.use(ElementUI,{size:'small'})
 // Vue.use(ElementUI)
 import './assets/gloable.css'
--- src/store/index.js
+++ src/store/index.js
@@ -3,15 +3,15 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
-export default new Vuex.Store({
-  state: {
-  },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+const store = new Vuex.Store({
+    state: {
+        currentPathName: ''
+    },
+    mutations: {
+        setPath (state) {
+            state.currentPathName = localStorage.getItem("currentPathName")
+        }
+    }
 })
+
+export default store
